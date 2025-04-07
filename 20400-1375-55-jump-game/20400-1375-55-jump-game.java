5058ec07d7c2f06ab9ff5cec1208046455771745
Harshit Sharma <harshitsharmasncp1.212@gmail.com>
2025-04-07 23:27:50 +0530

    Time: 3 ms, Memory: 46.1 MB - LeetHub

---
+++ 20400-1375-55-jump-game/20400-1375-55-jump-game.java
@@ -0,0 +1,15 @@
+public class Solution {
+
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false; // stuck before reaching this index
+            }
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return true;
+    }
+}
